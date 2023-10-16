67c6d88de43b0f685ace05aaebb3a27946a44966
Mark Silva <mark.silva@tesobe.com>
2023-10-17 00:55:07 +0800

    FEAT: JWS lib

---
+++ server/jws/sign.ts
@@ -0,0 +1,22 @@
+import * as jose from 'jose'
+
+async function run (): Promise<void>{
+  const secret = new TextEncoder().encode(
+    'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2'
+  )
+  const alg = 'HS256'
+
+  const jwt = await new jose.SignJWT({ 'urn:example:claim': true })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setIssuer('urn:example:issuer')
+    .setAudience('urn:example:audience')
+    .setExpirationTime('2h')
+    .sign(secret)
+  console.log(jwt)
+}
+
+(async ()=>{
+  console.log("Running sign..")
+  await run()
+})()
